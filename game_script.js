58cb87552dce960df883ab56e87d9b5d2fc127a3
Ezequiel <carlettieze@gmail.com>
2021-06-03 19:24:59 -0300

    se agregaron las funciones de crear nuevos elementos html

--- game_script.js
+++ game_script.js
@@ -1,5 +1,5 @@
-// image section
-const canvas = document.getElementById('backgroundImage')
+// Vars
+const gallery = document.getElementById('gallery')
 const fixedLeft = 450
 const TIME_REMAINING = 60 * 5 // 5 min
 const HIDDEN_OBJECT_FOUND = 15
@@ -7,6 +7,23 @@ const HIDDEN_OBJECT_NOT_FOUND = 10
 const numberMaxOfTrash = 8;
 
 let countItemsImg =0;
+
+
+// Main
+// Game STATE. 
+gameRunning();
+
+// Main
+
+
+
+/* 
+  ---------------------------------------------
+  ------------------Funciones------------------
+  ---------------------------------------------
+  ---------------------------------------------
+*/
+
 
 // Function for displaying images randomly
 function randomImages () {
@@ -27,17 +44,6 @@ function gameRunning(){
     
 }
 
-gameRunning();
-
-
-/* 
-  ---------------------------------------------
-  ------------------Funciones------------------
-  ---------------------------------------------
-  ---------------------------------------------
-*/
-
-
 // devuelve un numero entre el min y max
 function randomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -45,35 +51,37 @@ function randomNumber(min, max) {
 
 // crea dinamicamente un html tag <img>
 function createImgElement(){
+  var newDiv = createDivGallery();
+  gallery.appendChild(newDiv)
 
   var number = randomNumberTrash();
   var img = new Image(100, 100);
-  img.id= "img" + number;
-  img.src = 'images/trash/img' + number + '.png';
-  img.onclick = linkListener;
-  randomImageLocation(img);
-  canvas.appendChild(img);
+  setImgStyle(img,number);
+  newDiv.appendChild(img);
 
 }
 
 // onClick de cada imagen
 function linkListener() {
-  //alert(this.id);
   this.remove()
   countItemsImg--;
-}
-
-
-function randomImageLocation (img) {
-  img.style.display = 'block' // this line displays the image
-  img.style.position = 'relative' // to set the position of the picture (relative) to the background
-  const locationLeft = Math.floor(Math.random() * 900)
-  const locationTop = randomNumber(400, 600);
-  img.style.left = locationLeft + 'px';
-  img.style.top = locationTop  + 'px';
 }
 
 // devuelve un numero de 1 a numberMaxOfTrash(8 por ahora img8)
 function randomNumberTrash(){
   return randomNumber(1, numberMaxOfTrash);
+}
+
+function createDivGallery(){
+  var newDiv = document.createElement("div");
+  // Aca se mueve en el grid las posiciones del css 1,27... se pueden repetir las posiciones se podrian sacar
+  newDiv.className='gallery__item gallery__item--' + randomNumber(1,27);
+  return newDiv;
+}
+
+function setImgStyle(img, n){
+  img.id= "img" + n;
+  img.src = 'images/trash/img' + n + '.png';
+  img.class="gallery__img";
+  img.onclick = linkListener;
 }
